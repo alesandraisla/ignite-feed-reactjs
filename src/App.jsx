4d81cb37474b49e64488f76211fcae99903b9f30
AlesandraIsla <alesandramempis@gmail.com>
2022-09-16 21:22:27 -0300

    Componente Sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,26 @@
 import { Header } from "./components/Header";
 import { Post } from "./Post"
 import './global.css';
+import styles from './App.module.css';
+import { Sidebar } from "./components/Sidebar";
 
 function App() {
   return (
     <div>
       <Header/>
-      <Post
-        author="Isla"
-        content="Lorem ipsum dolor sit amet."
-      />
-      <Post 
-        author="Zoe"
-        content="Eu sou uma yorkshire que adoro passear."
-      />
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post
+            author="Isla"
+            content="Lorem ipsum dolor sit amet."
+          />
+          <Post 
+            author="Zoe"
+            content="Eu sou uma yorkshire que adoro passear."
+          />
+        </main>
+      </div>
     </div>
   )
 }
