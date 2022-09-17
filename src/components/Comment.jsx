c7f1a7d61d450d7f2a30065244540f088bb91ef9
AlesandraIsla <alesandramempis@gmail.com>
2022-09-17 20:18:04 -0300

    Component Comment

---
+++ src/components/Comment.jsx
@@ -0,0 +1,33 @@
+import styles from './Comment.module.css'
+import {ThumbsUp, Trash} from 'phosphor-react';
+
+export function Comment() {
+  return(
+    <div className={styles.comment}>
+      <img src="https://github.com/alesandraisla.png" alt="" />
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+        <header>
+          <div className={styles.authorAndTime}>
+            <strong>Alesandra Isla</strong>
+            <time title="16 de Setembro às 21:42" dateTime="2022-09-16 21:42:00">
+              Cerca de 1h atrás
+            </time>
+          </div>
+          <button title='Deletar comentário'>
+            <Trash size={20} />
+          </button>
+        </header>
+        <p>Muito bom!!!</p>
+        </div>
+        <footer>
+          <button>
+            <ThumbsUp />
+            Aplaudir <span>20</span>
+          </button>
+        </footer>
+
+      </div>
+    </div>
+  )
+}
