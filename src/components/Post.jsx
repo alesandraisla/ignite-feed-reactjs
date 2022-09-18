9550a92dcb91221a444884753031634dd9e16c91
AlesandraIsla <alesandramempis@gmail.com>
2022-09-17 21:28:48 -0300

    Componente avatar

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,3 +1,4 @@
+import { Avatar } from './Avatar';
 import { Comment } from './Comment';
 import styles from './Post.module.css';
 
@@ -7,7 +8,7 @@ export function Post() {
     <article className={styles.post}>
       <header>
         <div className={styles.author}>
-          <img className={styles.avatar} src="https://github.com/alesandraisla.png" />
+          <Avatar hasBorder={true} src="https://github.com/alesandraisla.png" />
           <div className={styles.authorInfo}>
             <strong>Alesandra Isla</strong>
             <span>Web Developer</span>
