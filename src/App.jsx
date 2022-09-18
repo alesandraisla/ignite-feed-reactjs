9157d9f61480ffe3897c58402600a7365910af17
AlesandraIsla <alesandramempis@gmail.com>
2022-09-18 16:13:04 -0300

    Trabalhando com datas e propriedades do Post

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,39 @@ import { Sidebar } from "./components/Sidebar";
 import './global.css';
 import styles from './App.module.css';
 
+//mockado
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'https://github.com/alesandraisla.png',
+      name: 'Alesandra Isla',
+      role: 'Software Developer'
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galera'},
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto'},
+      { type: 'link', content: 'yuna.design/tiposdecasas'},
+    ],
+    publishedAt: new Date('2022-09-18 15:30:00'),
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: 'https://github.com/lourivaldo.png',
+      name: 'Lourivaldo Vasconcelos',
+      role: 'Software Developer'
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galera'},
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto'},
+      { type: 'link', content: 'yuna.design/tiposdecasas'},
+    ],
+    publishedAt: new Date('2022-09-19 15:30:00'),
+  },
+]
+
+//iteracao - repetir alguma coisa, percorrer um array
 function App() {
   return (
     <div>
@@ -11,14 +44,15 @@ function App() {
       <div className={styles.wrapper}>
         <Sidebar />
         <main>
-          <Post
-            author="Isla"
-            content="Lorem ipsum dolor sit amet."
-          />
-          <Post 
-            author="Zoe"
-            content="Eu sou uma yorkshire que adoro passear."
-          />
+          {posts.map(post => {
+            return (
+              <Post
+                author={post.author}
+                content={post.content}
+                publishedAt={post.publishedAt}
+              />
+            )
+          })}
         </main>
       </div>
     </div>
