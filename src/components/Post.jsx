d954a7063eab5625ad9aa64e5d8786b35abb0d9c
AlesandraIsla <alesandramempis@gmail.com>
2022-09-18 19:34:32 -0300

    Inserindo comentários

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -3,8 +3,13 @@ import { Comment } from './Comment';
 import styles from './Post.module.css';
 import { format, formatDistanceToNow } from 'date-fns';
 import ptBR from 'date-fns/locale/pt-BR';
+import { useState } from 'react';
 
 export function Post({ author, publishedAt, content}) {
+const [comments, setComments] = useState([
+  1,
+  2,
+])
   const publishedDateFormatted = format(publishedAt, "d 'de' LLLL 'às' HH:mm'h'", {
     locale: ptBR,
   });
@@ -13,6 +18,12 @@ export function Post({ author, publishedAt, content}) {
     locale: ptBR,
     addSuffix: true,
   });
+
+  function handleCreateNewComment() {
+    event.preventDefault();
+    setComments([...comments, comments.length + 1]);
+    console.log()
+  }
 
   return(
     <article className={styles.post}>
@@ -37,7 +48,7 @@ export function Post({ author, publishedAt, content}) {
           }
         })}
       </div>
-      <form className={styles.commentForm}>
+      <form onSubmit={handleCreateNewComment} className={styles.commentForm}>
         <strong>Deixe seu feedback</strong>
         <textarea placeholder='Deixe um comentário'/>
         <footer>
@@ -45,9 +56,9 @@ export function Post({ author, publishedAt, content}) {
         </footer>
       </form>
       <div className={styles.commentList}>
-        <Comment />
-        <Comment />
-        <Comment />
+        {comments.map(comment => {
+          return <Comment />
+        })}
       </div>
     </article>
   )
